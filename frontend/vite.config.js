54f09d944f9ebca7cafcf82c089ee9b8f40dce55
Lalramnunmawia <80774787+papuia13@users.noreply.github.com>
2025-02-03 17:42:01 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server:{
     proxy:{"/api":{
       target: "http://localhost:5000",
